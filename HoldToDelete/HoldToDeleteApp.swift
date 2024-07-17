eb92a8efda228be110f42e4e19c937b43e645d00
Mohammed Rokon Uddin <rokon.uddin@monstar-lab.com>
2024-07-17 21:48:31 +0600

    Initial Commit

---
+++ HoldToDelete/HoldToDeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoldToDeleteApp.swift
+//  HoldToDelete
+//
+//  Created by Mohammed Rokon Uddin on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HoldToDeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
